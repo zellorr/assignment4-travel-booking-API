8cf1a4aaf35b92d06fc7681f374f8530404e6468
Ulan Akhmet <ulanahmet5@gmail.com>
2026-02-01 20:22:22 +0500

    `InvalidInputException` is a custom runtime exception used to signal invalid or incorrect input data.

---
+++ src/exception/InvalidInputException.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+    public InvalidInputException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
